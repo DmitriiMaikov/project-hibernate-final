77721f6ffd409697a0955f829ea10e68d22ceddb
DmitriiMaikov <dmitriimaikov15@gmail.com>
2024-06-18 15:36:41 +0300

    all work

---
+++ src/main/java/com/javarush/redis/Language.java
@@ -0,0 +1,17 @@
+package com.javarush.redis;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+}
